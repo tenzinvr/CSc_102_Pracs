88e25c18ba5ebcbab9ad2d39e940704e096d9ec7
Tenzin_VR <tenzinvr@gmail.com>
2018-07-18 14:33:31 +0200

    Commented

--- P1T2-3/ConsoleApp1/Program.cs
+++ P1T2-3/ConsoleApp1/Program.cs
@@ -10,18 +10,22 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Please enter the frequency of the drips (in seconds):");
+            //Get drip frequency from user
+            Console.WriteLine("Please enter the frequency of the drips (in seconds):");
 			int intFreq = Convert.ToInt32(Console.ReadLine());
 
-			double dblHr = 3600/intFreq*0.00025;
+            //Calculate volume of water wasted per period
+            double dblHr = 3600/intFreq*0.00025;
 			double dblDay = 86400 / intFreq * 0.00025;
 			double dblWk = 604800 / intFreq * 0.00025;
 
-			Console.WriteLine("You are wasting;");
+            //Display results
+            Console.WriteLine("You are wasting;");
 			Console.WriteLine(dblHr.ToString() + " liters per hour");
 			Console.WriteLine(dblDay.ToString() + " liters per day");
 			Console.WriteLine(dblWk.ToString() + " liters per week");
 
+            //Hold console
 			Console.ReadLine();
 		}
 	}
--- P1T2-3/P1T3/Program.cs
+++ P1T2-3/P1T3/Program.cs
@@ -10,18 +10,23 @@ namespace P1T3
 	{
 		static void Main(string[] args)
 		{
+			//Get drip frequency from user
 			Console.WriteLine("Please enter the frequency of the drips (in seconds):");
 			int intFreq = Convert.ToInt32(Console.ReadLine());
 
+			//Calculate volume of water wasted per period
 			double dblHr = 3600 / intFreq * 0.00025;
 			double dblDay = 86400 / intFreq * 0.00025;
 			double dblWk = 604800 / intFreq * 0.00025;
 
-			Console.WriteLine("You are wasting;");
-			Console.WriteLine(Convert.ToInt32(dblHr).ToString() + " glasses of water per hour");
-			Console.WriteLine(Convert.ToInt32(dblDay).ToString() + " glasses of water per day");
-			Console.WriteLine(Convert.ToInt32(dblWk).ToString() + " glasses of water per week");
+            //Display results
+            Console.WriteLine("You are wasting;");
+            //Divide result by 4 and convert to an integer to have number of glasses
+			Console.WriteLine(Convert.ToInt32(dblHr/4).ToString() + " glasses of water per hour");
+			Console.WriteLine(Convert.ToInt32(dblDay/4).ToString() + " glasses of water per day");
+			Console.WriteLine(Convert.ToInt32(dblWk/4).ToString() + " glasses of water per week");
 
+            //Hold console
 			Console.ReadLine();
 		}
 	}
