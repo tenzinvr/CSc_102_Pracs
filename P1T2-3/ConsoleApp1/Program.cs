71a8c18ab9539758781630b47bddf3959ec9e8dc
Tenzin_VR <tenzinvr@gmail.com>
2018-07-18 12:45:50 +0200

    V.1

---
+++ P1T2-3/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter the frequency of the drips (in seconds):");
+			int intFreq = Convert.ToInt32(Console.ReadLine());
+			double dblHr = 3600/intFreq*0.00025;
+			double dblDay = 86400 / intFreq * 0.00025;
+			double dblWk = 604800 / intFreq * 0.00025;
+			Console.WriteLine("You are wasting;");
+			Console.WriteLine(dblHr.ToString() + " liters per hour");
+			Console.WriteLine(dblDay.ToString() + " liters per day");
+			Console.WriteLine(dblWk.ToString() + " liters per week");
+			Console.ReadLine();
+		}
+	}
+}
