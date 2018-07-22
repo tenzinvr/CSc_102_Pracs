57132623a6078b1b78f71a29e72ac1dcf0c79492
Tenzin_VR <tenzinvr@gmail.com>
2018-07-22 10:41:11 +0200

    Task 4 added

---
+++ P1T2-3/InputValidation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidation
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Get drip frequency from user
+            Console.WriteLine("Please enter the frequency of the drips (in seconds):");
+            int intFreq;
+            //Display error message and restart if input invalid
+            if (!(int.TryParse(Console.ReadLine(), out intFreq)))
+            {
+                Console.WriteLine("Error. Please use digits only.");
+                Program.Main();
+            }
+
+            //Calculate volume of water wasted per period
+            double dblHr = 3600 / intFreq * 0.00025;
+            double dblDay = 86400 / intFreq * 0.00025;
+            double dblWk = 604800 / intFreq * 0.00025;
+
+            //Display results
+            Console.WriteLine("You are wasting;");
+            Console.WriteLine(dblHr.ToString() + " liters per hour");
+            Console.WriteLine(dblDay.ToString() + " liters per day");
+            Console.WriteLine(dblWk.ToString() + " liters per week");
+
+            //Hold console
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P1T4/P1T4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get drip frequency from user
+            Console.WriteLine("Please enter the frequency of the drips (in seconds):");
+            string strFreq = Console.ReadLine();
+            if int.TryParse(strFreq):
+            {
+                int intFreq = Convert.ToInt32(strFreq);
+            }
+            else:
+            {
+                Console.WriteLine("Error. Incorrect format. Please use integers only.");
+                Main();
+            }
+
+            //Calculate volume of water wasted per period
+            double dblHr = 3600 / intFreq * 0.00025;
+            double dblDay = 86400 / intFreq * 0.00025;
+            double dblWk = 604800 / intFreq * 0.00025;
+
+            //Display results
+            Console.WriteLine("You are wasting;");
+            Console.WriteLine(dblHr.ToString() + " liters per hour");
+            Console.WriteLine(dblDay.ToString() + " liters per day");
+            Console.WriteLine(dblWk.ToString() + " liters per week");
+
+            //Hold console
+            Console.ReadLine();
+        }
+    }
+}
