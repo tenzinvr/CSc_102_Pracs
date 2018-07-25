5511a8fbcdae70ba44bc23e85590d9d1a8655248
Tenzin_VR <tenzinvr@gmail.com>
2018-07-25 13:26:05 +0200

    Finish ifs

--- Prac02_Classwork/TaxesCalculator/Program.cs
+++ Prac02_Classwork/TaxesCalculator/Program.cs
@@ -19,23 +19,31 @@ namespace TaxesCalculator
 
         }
 
-        static string Calc(int age, int sal)
+        static int Calc(int age, int sal)
         {
             //int red = 0;
-            int lsRed[];
-            int lsRed1[10] = { 0, 13, 250, 620, 600, 600, 700, 500, 400, 2950 };
+            int lsBase[] = { 0, 13, 250, 620, 1200, 2100, 2700, 8000, 16000, 20000 };
+            int lsRed1[] = new int[ 0, 13, 250, 620, 600, 600, 700, 500, 400, 2950 ];
             int lsRed2[10] = { 0, 13, 250, 620, 800, 900, 900, 700, 600, 3000 };
-
-            if (age < 65) { int lsRed = 0; }
-            else if ((age >= 65) && (age <= 75)) { lsRed = lsRed1; }
-            else { lsRed = lsRed2;  }
 
             if ((sal > 0) && (sal < 5000)) { sal = 0; }
             else if ((sal >= 5000) && (sal < 6500)) { sal = 1; }
+            else if ((sal >= 6500) && (sal < 7799)) { sal = 2; }
+            else if ((sal >= 7799) && (sal < 9999)) { sal = 3; }
+            else if ((sal >= 9999) && (sal < 13499)) { sal = 4; }
+            else if ((sal >= 13499) && (sal < 17899)) { sal = 5; }
+            else if ((sal >= 17899) && (sal < 19999)) { sal = 6; }
+            else if ((sal >= 19999) && (sal < 39999)) { sal = 7; }
+            else if ((sal >= 39999) && (sal < 59999)) { sal = 8; }
+            else { sal = 9; }
 
+            int intTax = 0;
+            
+            if (age < 65) { intTax = lsBase[sal]; }
+            else if ((age >= 65) && (age <= 75)) { intTax = lsBase[sal] - lsRed1[sal]; }
+            else { intTax = lsBase[sal] - lsRed2[sal]; }
 
-            string strTax = "";
-            return strTax;
+            return intTax;
         }
 
         static void InpErr(string strErr = "valid digits")
