2611fb253909660f2853ee9b7f6120d7483b80d1
Tenzin_VR <tenzinvr@gmail.com>
2018-07-25 14:35:12 +0200

    V1.0

--- Prac02_Classwork/TaxesCalculator/Program.cs
+++ Prac02_Classwork/TaxesCalculator/Program.cs
@@ -14,7 +14,7 @@ namespace TaxesCalculator
             if (!(int.TryParse(Console.ReadLine(), out int intAge))) { InpErr("integers"); Program.Main(); }
             Console.WriteLine("Please enter your monthly salary:");
             if (!(int.TryParse(Console.ReadLine(), out int intSal))) { InpErr("integers"); Program.Main(); }
-            Console.WriteLine("Your monthly tax is ", Calc(intAge, intSal));
+            Console.WriteLine("Your monthly tax is " + Calc(intAge, intSal).ToString() );
             Console.ReadLine();
 
         }
@@ -22,9 +22,9 @@ namespace TaxesCalculator
         static int Calc(int age, int sal)
         {
             //int red = 0;
-            int lsBase[] = { 0, 13, 250, 620, 1200, 2100, 2700, 8000, 16000, 20000 };
-            int lsRed1[] = new int[ 0, 13, 250, 620, 600, 600, 700, 500, 400, 2950 ];
-            int lsRed2[10] = { 0, 13, 250, 620, 800, 900, 900, 700, 600, 3000 };
+            int[] lsBase = new int[10] { 0, 13, 250, 620, 1200, 2100, 2700, 8000, 16000, 20000 };
+            int[] lsRed1 = new int[] { 0, 13, 250, 620, 600, 600, 700, 500, 400, 2950 };
+            int[] lsRed2 = new int[] { 0, 13, 250, 620, 800, 900, 900, 700, 600, 3000 };
 
             if ((sal > 0) && (sal < 5000)) { sal = 0; }
             else if ((sal >= 5000) && (sal < 6500)) { sal = 1; }
@@ -42,7 +42,7 @@ namespace TaxesCalculator
             if (age < 65) { intTax = lsBase[sal]; }
             else if ((age >= 65) && (age <= 75)) { intTax = lsBase[sal] - lsRed1[sal]; }
             else { intTax = lsBase[sal] - lsRed2[sal]; }
-
+            Console.WriteLine(intTax.ToString());
             return intTax;
         }
 
