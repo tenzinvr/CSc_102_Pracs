9a11f43b4dca43f4e687e22c4484c42a08572795
Tenzin_VR <tenzinvr@gmail.com>
2018-07-29 18:44:51 +0200

    V2.0

--- Prac02_Classwork/ChangeCalculator/Program.cs
+++ Prac02_Classwork/ChangeCalculator/Program.cs
@@ -13,44 +13,33 @@ namespace ChangeCalculator
             Console.WriteLine("Please enter change required:");
             double dblChan = Convert.ToDouble(Console.ReadLine());
 
-            while (dblChan > 0)
+            List<double> lsChan = new List<double>();
+            lsChan.Add(200.0);
+            lsChan.Add(100.0);
+            lsChan.Add(50.0);
+            lsChan.Add(20.0);
+            lsChan.Add(10.0);
+            lsChan.Add(0.50);
+            lsChan.Add(0.20);
+            lsChan.Add(0.10);
+            lsChan.Add(0.05);
+            lsChan.Add(0.02);
+            lsChan.Add(0.01);
+
+            List<int> lsCnt = new List<int>(5);
+
+            for (int x = 0; x <= 10; x++)
             {
-                //int cnt200 = 0, cnt100 = 0, cnt50 = 0, cnt20 = 0, cnt10 = 0, cnt5 = 0, cnt2 = 0, cnt1 = 0, cnt05 = 0, cnt02 = 0, cnt01 = 0, cnt005 = 0, cnt002 = 0, cnt001 = 0;
+                lsCnt.Add(0);
 
-                /*if (dblChan >= 200) { dblChan = dblChan - 200; cnt200 += 1; }
-                else if (dblChan >= 100) { dblChan = dblChan - 100; cnt100 += 1; }
-                else if (dblChan >= 50) { dblChan = dblChan - 50; cnt50 += 1; }
-                else if (dblChan >= 20) { dblChan = dblChan - 20; cnt20 += 1; }
-                else if (dblChan >= 10) { dblChan = dblChan - 10; cnt10 += 1; }
-                else if (dblChan >= 5) { dblChan = dblChan - 5; cnt5 += 1; }
-                else if (dblChan >= 2) { dblChan = dblChan - 2; cnt2 += 1; }
-                else if (dblChan >= 1) { dblChan = dblChan - 1; cnt1 += 1; }
-                else if (dblChan >= 0.5) { dblChan = dblChan - 0.5; cnt05 += 1; }
-                else if (dblChan >= 0.2) { dblChan = dblChan - 0.2; cnt02 += 1; }
-                else if (dblChan >= 0.1) { dblChan = dblChan - 0.1; cnt01 += 1; }
-                else if (dblChan >= 0.05) { dblChan = dblChan - 0.05; cnt005 += 1; }
-                else if (dblChan >= 0.02) { dblChan = dblChan - 0.02; cnt002 += 1; }
-                else if (dblChan >= 0.01) { dblChan = dblChan - 0.01; cnt001 += 1; }
-                else { break; }*/
-
-                double[] lsChan = new double[] { 200, 100, 50, 20, 10 };
-                int[] lsCnt = new int[] { };
-                for (int x = 0; x == 5; x += 1 )
+                while (dblChan >= lsChan[x])
                 {
-                    while (dblChan >= lsChan[x])
-                    {
-                        dblChan = dblChan - lsChan[x];
-                        lsCnt[x] += 1;
-                    }
+                    dblChan = dblChan - lsChan[x];
+                    lsCnt[x] = lsCnt[x] += 1;
                 }
-
-                Console.WriteLine("Change Succ");
-                Console.WriteLine("Return {0} x R200", lsCnt[0].ToString());
-                Console.WriteLine("Return {0} x R100", lsCnt[1].ToString());
+                Console.WriteLine("Return {0} x R{1}", lsCnt[x].ToString(), lsChan[x]);
             }
-
-
-
+            Console.ReadKey();
         }
     }
 }
